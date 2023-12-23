9db0de2c204ad384a93704e93cb17695a791a03e
Boris Manzov <125819932+Universe8888@users.noreply.github.com>
2023-12-23 22:37:27 +0200

    Update alfa v.1
    update

--- script.js
+++ script.js
@@ -2,8 +2,26 @@ const startButton = document.getElementById('startButton');
 const targetDateTime = document.getElementById('targetDateTime');
 const countdownDisplay = document.getElementById('countdownDisplay');
 
+function isValidDate(d) {
+  return d instanceof Date && !isNaN(d);
+}
+
 startButton.addEventListener('click', function() {
-    const targetDate = new Date(targetDateTime.value);
+    const targetValue = targetDateTime.value;
+    const targetDate = new Date(targetValue.replace(/-/g, '/'));
+    
+    // Check if the date is valid
+    if (!isValidDate(targetDate)) {
+        alert("Please enter the date and time in the correct format (dd/mm/yyyy hh:mm) and ensure it is a future date.");
+        return; // Exit the function if the date is not valid
+    }
+
+    // Check if the date is in the future
+    if (targetDate < new Date()) {
+        alert("Please enter a future date and time.");
+        return; // Exit the function if the date is not in the future
+    }
+
     const interval = setInterval(function() {
         const now = new Date().getTime();
         const distance = targetDate - now;
